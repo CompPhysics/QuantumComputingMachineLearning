87cadac0662da55995f965c050ecb92f402129de
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2025-02-24 12:49:59 +0100

    more updates

--- doc/src/week6/make.sh
+++ doc/src/week6/make.sh
@@ -50,6 +50,17 @@ system doconce format html $name --html_style=bootstrap --pygments_html_style=de
 system doconce format ipynb $name $opt
 
 
+# LaTeX Beamer slides
+beamertheme=red_plain
+system doconce format pdflatex $name --latex_title_layout=beamer --latex_table_format=footnotesize $opt
+system doconce ptex2tex $name envir=minted
+# Add special packages
+doconce subst "% Add user's preamble" "\g<1>\n\\usepackage{simplewick}" $name.tex
+system doconce slides_beamer $name --beamer_slide_theme=$beamertheme
+system pdflatex -shell-escape ${name}
+system pdflatex -shell-escape ${name}
+cp $name.pdf ${name}.pdf
+cp $name.tex ${name}.tex
 
 
 # Publish
