3e7252de2da26dd886c839c52b12fd3e779f0b9b
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2026-03-06 16:09:37 +0100

    update

--- doc/src/week8/make.sh
+++ doc/src/week8/make.sh
@@ -27,27 +27,22 @@ opt="--no_mako"
 rm -f *.aux
 
 
-html=${name}-reveal
-system doconce format html $name --pygments_html_style=perldoc --keep_pygments_html_bg --html_links_in_new_window --html_output=$html $opt
-system doconce slides_html $html reveal --html_slide_theme=beige
-
-# Plain HTML documents
-
-html=${name}-solarized
-system doconce format html $name --pygments_html_style=perldoc --html_style=solarized3 --html_links_in_new_window --html_output=$html $opt
-system doconce split_html $html.html --method=space10
-
-html=${name}
-system doconce format html $name --pygments_html_style=default --html_style=bloodish --html_links_in_new_window --html_output=$html $opt
-system doconce split_html $html.html --method=space10
-
-# Bootstrap style
-html=${name}-bs
-system doconce format html $name --html_style=bootstrap --pygments_html_style=default --html_admon=bootstrap_panel --html_output=$html $opt
-#system doconce split_html $html.html --method=split --pagination --nav_button=bottom
 
 # IPython notebook
 system doconce format ipynb $name $opt
+
+
+# LaTeX Beamer slides
+beamertheme=red_plain
+system doconce format pdflatex $name --latex_title_layout=beamer --latex_table_format=footnotesize $opt
+system doconce ptex2tex $name envir=minted
+# Add special packages
+doconce subst "% Add user's preamble" "\g<1>\n\\usepackage{simplewick}" $name.tex
+system doconce slides_beamer $name --beamer_slide_theme=$beamertheme
+system pdflatex -shell-escape ${name}
+system pdflatex -shell-escape ${name}
+cp $name.pdf ${name}.pdf
+cp $name.tex ${name}.tex
 
 
 # Publish
